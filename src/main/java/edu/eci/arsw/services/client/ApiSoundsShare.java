6b441332aa70ce72bcb9b91ef3541884c7bde6af
unknown <fernando.barrera@labinfo.is.escuelaing.edu.co>
2020-03-09 17:21:18 -0500

    paso a spring:wolf:

---
+++ src/main/java/edu/eci/arsw/services/client/ApiSoundsShare.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.services.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author fernando.barrera
+ */
+@Controller
+@SpringBootApplication
+public class ApiSoundsShare {
+    @RequestMapping("/")
+    public String welcome(){
+        return "sala";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSoundsShare.class, args);
+    }
+
+}
