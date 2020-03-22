91015e079e1461fe3a6b274122a067f89840be4a
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-03-22 17:54:50 -0500

    Avance Api

--- src/main/java/edu/eci/arsw/controllers/SoundShareAPIController.java
+++ src/main/java/edu/eci/arsw/controllers/SoundShareAPIController.java
@@ -1,5 +1,6 @@
 package edu.eci.arsw.controllers;
 
+import edu.eci.arsw.services.client.impl.ServiciosSoundShareImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class SoundShareAPIController {
-
+    @Autowired
+    ServiciosSoundShareImpl ser = null;
 
 
 }
