d1402a992d3839cf65f2919937846a0665ed784c
car-cas <carlos.castaneda-l@mail.escuelaing.edu.co>
2020-05-04 17:04:35 -0500

    link heroku

--- src/main/resources/templates/js/apiclient.js
+++ src/main/resources/templates/js/apiclient.js
@@ -3,8 +3,8 @@ var apiclient = (function () {
         getSongs : function (callback){
             console.log("entrada");
             jQuery.ajax({
-                url : 'http://localhost:8080/canciones',
-                //url: 'https://soundsharearsw.herokuapp.com/canciones',
+                //url : 'http://localhost:8080/canciones',
+                url: 'https://soundsharearsw.herokuapp.com/canciones',
                 method: 'GET',
                 success: function (respuesta) {
                     callback(respuesta);
