eb95a25ae67494b61d81f033dab778ae72d8016d
fernando <f9830@hotmail.com>
2020-05-13 23:47:08 -0500

    pequeño arreglo hope :wolf:

--- src/main/resources/templates/js/websocket.js
+++ src/main/resources/templates/js/websocket.js
@@ -22,9 +22,6 @@ var websocket = (function () {
 		user = nick;
         stompClient.connect({}, function (frame) {
 			console.log('Connected: ' + frame);
-			console.log('aquiiiiiiiiiiiiiiiiiiiiiiii');
-			stompClient.send('/app/sala/'+id+'/unir/'+nick,{},'');
-			connectAndSubscribeMain();
             stompClient.subscribe('/topic/sala/'+id,function(eventbody){
 				console.log(localStorage.getItem("nameRoom"));
 				console.log(eventbody.body);
@@ -41,8 +38,13 @@ var websocket = (function () {
 
 				
             });
+			console.log('aquiiiiiiiiiiiiiiiiiiiiiiii');
+			connectAndSubscribeMain();
+			stompClient.send('/app/sala/'+id+'/unir/'+nick,{},'');
 			
-		
+			
+			
+			
         });
     };
 	function desconnectUser(id,nick){
@@ -57,7 +59,6 @@ var websocket = (function () {
 		console.log('conecto xd');
 		stompClient.connect({}, function (frame) {
 			console.log('Connected: ' + frame);
-			stompClient.send('/app/sala/'+sala+'/cancionActual/'+' '+'/seg/'+0,{},'');
             stompClient.subscribe('/topic/sala/'+sala+'/cancionActual',function(eventbody){
 				var datos = JSON.parse(eventbody.body);
 				console.log(datos);
@@ -74,6 +75,7 @@ var websocket = (function () {
 					apiyoutube.onYouTubeIframeAPIReady(datos[0],minut+4);
 				}
             });	
+			stompClient.send('/app/sala/'+sala+'/cancionActual/'+' '+'/seg/'+0,{},'');
         });
 	}
 	
