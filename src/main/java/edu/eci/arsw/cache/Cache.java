635041c730c0011508b56168d73164382678e8e3
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-05-15 17:20:02 -0500

    correccion clase cache crear diciconario canciones

--- src/main/java/edu/eci/arsw/cache/Cache.java
+++ src/main/java/edu/eci/arsw/cache/Cache.java
@@ -1,4 +1,10 @@
 package edu.eci.arsw.cache;
 
+import java.util.Hashtable;
+
 public class Cache {
+    public static void Cache(){
+        Hashtable<String, Object> canciones = new Hashtable<String, Object>();
+
+    }
 }
