1399fb1b41a1c2cf42ec6b3c3f9dfd16438ee5ea
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-05-15 17:21:11 -0500

    correccion clase cache diciconario sala e user

--- src/main/java/edu/eci/arsw/cache/Cache.java
+++ src/main/java/edu/eci/arsw/cache/Cache.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 public class Cache {
     public static void Cache(){
         Hashtable<String, Object> canciones = new Hashtable<String, Object>();
-
+        Hashtable<String, Object> salas = new Hashtable<String, Object>();
+        Hashtable<String, Object> users = new Hashtable<String, Object>();
     }
 }
