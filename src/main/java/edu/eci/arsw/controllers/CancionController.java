abe5d7f391c3dd877111c65f27dcb69d01423f4b
luisalejandrojaramillo <luis.jaramillo-r@mail.escuelaing.edu.co>
2020-05-18 21:56:04 -0500

    cache avance

--- src/main/java/edu/eci/arsw/controllers/CancionController.java
+++ src/main/java/edu/eci/arsw/controllers/CancionController.java
@@ -68,7 +68,7 @@ public class CancionController {
     @RequestMapping(path ="/{id}",method = RequestMethod.GET)
     public ResponseEntity<?> getCancionById(@PathVariable ("id") String id){
         try {
-            return new ResponseEntity<>(services.getCancionById(id),HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(cache.getCancionById(id),HttpStatus.ACCEPTED);
             //meodo que dado el id recorne la cancion con esa id
         } catch (Exception e) {
 			Logger.getLogger(ExceptionServiciosReserva.class.getName()).log(Level.SEVERE, null, e);
@@ -78,7 +78,7 @@ public class CancionController {
     @RequestMapping(path ="/getByName/{nombre}",method = RequestMethod.GET)
     public ResponseEntity<?> getCancionByNombre(@PathVariable ("nombre") String nombre){
         try {
-            return new ResponseEntity<>(services.getCancionByName(nombre),HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(cache.getCancionByNombre(nombre),HttpStatus.ACCEPTED);
         } catch (Exception e) {
 			Logger.getLogger(ExceptionServiciosReserva.class.getName()).log(Level.SEVERE, null, e);
 			return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
@@ -87,7 +87,7 @@ public class CancionController {
     @RequestMapping(path ="/{id}/users",method = RequestMethod.GET)
     public ResponseEntity<?> getUsersThatHaveCancion(@PathVariable ("id") String id){
         try {
-            return new ResponseEntity<>(services.getUsersThatHaveCancion(id),HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(cache.getUsersThatHaveCancion(id),HttpStatus.ACCEPTED);
         }catch (Exception e) {
 			Logger.getLogger(ExceptionServiciosReserva.class.getName()).log(Level.SEVERE, null, e);
 			return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
@@ -96,7 +96,7 @@ public class CancionController {
     @RequestMapping(path ="/{id}/salas",method = RequestMethod.GET)
     public ResponseEntity<?>getSalasThatHaveCancion(@PathVariable ("id") String id){
         try {
-            return new ResponseEntity<>(services.getSalasThatHaveCancion(id),HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(cache.getSalasThatHaveCancion(id),HttpStatus.ACCEPTED);
         } catch (Exception e) {
 			Logger.getLogger(ExceptionServiciosReserva.class.getName()).log(Level.SEVERE, null, e);
 			return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
